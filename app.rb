039cfbc1f89c8b31550d4cdd8f08966ae5516276
Nia Bert <nbert14@gmail.com>
2019-08-25 13:40:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,8 +3,8 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   
   configure do
-  enable :sessions
-  set :session_secret, "secret"
+    enable :sessions
+    set :session_secret, "secret"
   end
   
   get '/' do
